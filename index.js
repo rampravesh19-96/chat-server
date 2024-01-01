929ff6e204a71a76336fb92ecb5f8d487de64196
ram pravesh singh <rampraveshsingh1996@gmail.com>
2024-01-01 22:43:52 +0530

    sd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors : {
-        origin : "https://chat-client-amber.vercel.app/",
+        origin : "https://chat-client-amber.vercel.app",
         methods : ["GET","POST"]
     },
 })
