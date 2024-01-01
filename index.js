412ede18fffee85bf952b063aa7b124f28b0938a
ram pravesh singh <rampraveshsingh1996@gmail.com>
2024-01-01 22:53:57 +0530

    sdf

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ app.use(cors())
 
 const server = http.createServer(app)
 
-const io = new Server(server);
-io.origins("https://chat-client-amber.vercel.app:*");
-
+const io = new Server(server,{
+    cors : {
+        origin : "https://chat-client-amber.vercel.app",
+        methods : ["GET","POST"]
+    },
+})
 
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
